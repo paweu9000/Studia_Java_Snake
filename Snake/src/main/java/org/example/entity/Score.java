56d6a5a2b54461d46cae3d9b1143f8ce4901640e
Janosik2 <167571684+Janosik2@users.noreply.github.com>
2024-05-22 19:30:56 +0200

    add Score

---
+++ Snake/src/main/java/org/example/entity/Score.java
@@ -0,0 +1,34 @@
+package org.example.entity;
+
+
+import com.raylib.Raylib;
+
+import static com.raylib.Jaylib.GRAY;
+import static com.raylib.Jaylib.GREEN;
+import static com.raylib.Raylib.*;
+
+public class Score extends Entity{
+    Integer score = 0;
+
+    public void addScore() {
+        this.score += 1;
+    };
+
+    @Override
+    public void update() {
+        addScore();
+    }
+
+    @Override
+    public void draw() {
+
+        if(score>10){
+            Raylib.DrawText("Score: "+score.toString(),0,0,35, GREEN);
+        }else{
+            Raylib.DrawText("Score: "+score.toString(),0,0,30, GRAY);
+        }
+        Raylib.DrawText("Time: "+ GetTime(),0,40,0, GRAY);
+
+
+    }
+}
